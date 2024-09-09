824a0709be5066323abc55c50d1063eb7881c8ec
Sahil Raj Srivastava <111967330+codewith-sahil1@users.noreply.github.com>
2024-09-09 08:47:16 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,108 @@
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+
+
+
+let search = document.querySelector('.search');
+document.querySelector('#search').onclick=() =>{
+    search.classList.toggle('active');
+}
+
+
+var swiper = new Swiper(".product-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+var swiper = new Swiper(".blogs-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation:{
+        nextE1 :".swiper-button-next",
+        prevE1 :".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 1,
+      },
+      1024: {
+        slidesPerView: 1,
+      },
+    },
+  });
+
+  var swiper = new Swiper(".review-row", {
+    spaceBetween: 30,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+        delay:9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1024: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  function redirectToWhatsApp() {
+    // Define the WhatsApp number and message
+    const phoneNumber = '1234567890'; // Replace with your WhatsApp number
+    const message = 'I want to shop from your website.';
+
+    // Create the WhatsApp URL
+    const whatsappURL = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodeURIComponent(message)}`;
+
+    // Redirect to WhatsApp
+    window.location.href = whatsappURL;
+}
+
+// Add an event listener to all elements with the "shop-button" class
+const shopButtons = document.querySelectorAll('.shop-button');
+shopButtons.forEach(button => {
+    button.addEventListener('click', redirectToWhatsApp);
+});
